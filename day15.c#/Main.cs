9d8edbd170ad504f856fb1baf9a4fa4fb080d8c1
ms747 <mayur.shah.ha@gmail.com>
2020-12-21 21:40:27 +0530

    Day 15

--- day15.c#/Main.cs
+++ day15.c#/Main.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 
 class MainClass {
-
     class NumberInfo {
-
         public int count;
         public int first;
         public int second;
@@ -39,47 +37,32 @@ class MainClass {
                 visits[starting[i - 1]] = new NumberInfo(0, i, i);
                 lastSpokenNumber = starting[i - 1];
             } else {
-
                 if (visits.ContainsKey(lastSpokenNumber)) {
-
                     if (visits[lastSpokenNumber].count == 0) {
-
                         lastSpokenNumber = 0;
-
                         if (visits.ContainsKey(lastSpokenNumber)) {
                             visits[lastSpokenNumber].NewPosition(i);
-                        }
-
-                        else {
+                        } else {
                             visits[lastSpokenNumber] = new NumberInfo(0, i, i);
                         }
                     }
 
                     else {
-
                         lastSpokenNumber =
                             visits[lastSpokenNumber].second - visits[lastSpokenNumber].first;
 
                         if (visits.ContainsKey(lastSpokenNumber)) {
                             visits[lastSpokenNumber].NewPosition(i);
-                        }
-
-                        else {
+                        } else {
                             visits[lastSpokenNumber] = new NumberInfo(0, i, i);
                         }
-
                     }
-
                 }
-
             }
-
             if (i == 2020) {
                 Console.WriteLine("Part 1 : {0}", lastSpokenNumber);
             }
-
         }
-
         Console.WriteLine("Part 2 : {0}", lastSpokenNumber);
     }
 }
