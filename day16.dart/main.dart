5363f19b7d61f5ff93331ce42a4ff81920a709a2
ms747 <mayur.shah.ha@gmail.com>
2020-12-23 17:55:01 +0530

    Day 16

--- day16.dart/main.dart
+++ day16.dart/main.dart
@@ -1,70 +1,148 @@
 import 'dart:io';
 
-void main() async {
-    File file = new File('sample.txt');
+class Entry {
+    String key;
+    List<int> value;
+
+    Entry(String key) {
+        this.key = key;
+        this.value = new List();
+    }
+
+    void add(int value) {
+        this.value.add(value);
+    }
+
+    @override
+    String toString() {
+        return "${this.key} - ${this.value}";
+    }
+}
+
+List<int> getColumn(List<int> nt, List<int> yt, List<int> ig, int at, int id) {
+    List<int> temp = new List();
+
+    for (int i = id; i < nt.length; i += at) {
+        temp.add(nt[i]);
+    }
+
+    if (!temp.contains(yt[id])) {
+        temp.add(yt[id]);
+    }
+
+    for (int ignore in ig) {
+        temp.remove(ignore);
+    }
+
+    return temp;
+}
+
+void main(List<String> args) async {
+    if (args.length < 1) {
+        print("input.txt not provided");
+        exit(1);
+    }
+
+    File file = new File(args[0]);
     String contents = await file.readAsString();
 
-
     List<String> values = contents.trim().split("\n\n");
-
     RegExp rulesRegExp = RegExp(r"(.*): (\d+)-(\d+) \w+ (\d+)-(\d+)");
 
-    Map<String, Set<int>> rules = new Map();
-    List<int> nearByTickets = new List();
-    List<int> yourTickets;
 
+    Map<String, Set<int>> rules = new Map();
     for (String i in values[0].split('\n')) {
-        for (var y in rulesRegExp.allMatches(i)) {
+        for (RegExpMatch y in rulesRegExp.allMatches(i)) {
+            Set<int> setHolder = {};
             String attribute = y.group(1);
-            print(attribute);
-            int start1
-            // int start = int.parse(y.group(1));
-            // int end = int.parse(y.group(2));
-            // for (int j = start; j <= end; j++) {
-            //     rules.add(j);
-            // }
+
+            int start1 = int.parse(y.group(2));
+            int end1 = int.parse(y.group(3));
+            int start2 = int.parse(y.group(4));
+            int end2 = int.parse(y.group(5));
+
+            for (int i = start1; i <= end1; i++) {
+                setHolder.add(i);
+            }
+
+            for (int i = start2; i <= end2; i++) {
+                setHolder.add(i);
+            }
+
+            rules[attribute] = setHolder;
         }
     }
 
-    // yourTickets = values[1]
-    //         .split("\n")
-    //         .skip(1)
-    //         .elementAt(0)
-    //         .split(",")
-    //         .map((val) { return int.parse(val); })
-    //         .toList()
-    //         ;
 
-    // var colCount = values[2]
-    //         .split("\n")
-    //         .skip(1)
-    //         .elementAt(0)
-    //         .split(",")
-    //         .length;
+    List<int> yourTickets = values[1]
+            .split("\n")
+            .skip(1)
+            .elementAt(0)
+            .split(",")
+            .map(int.parse)
+            .toList();
 
-    // for (var i in values[2].split("\n").skip(1)) {
-    //     List<int> ticketRow = i
-    //             .split(",")
-    //             .map((val) { return int.parse(val); })
-    //             .toList();
-    //     nearByTickets += ticketRow;
-    // }
 
-    // int sum = 0;
-    // List<int> indicesToBeRemove = new List();
-    // for (int i in nearByTickets) {
-    //     if (!rules.contains(i)) {
-    //         sum += i;
-    //         indicesToBeRemove.add(i);
-    //     }
-    // }
+    List<int> nearByTickets = new List();
+    for (String i in values[2].split("\n").skip(1)) {
+        List<int> ticketRow = i
+                .split(",")
+                .map(int.parse)
+                .toList();
+        nearByTickets += ticketRow;
+    }
 
-    // for(int i in indicesToBeRemove) {
-    //     nearByTickets.remove(i);
-    // }
+    Set<int> flatRules = rules
+            .entries
+            .fold(<int>{}, (acc, item) {
+                acc.addAll(item.value); return acc;
+            });
 
-    // print(sum);
+    int sum = 0;
+    List<int> indicesToBeRemove = new List();
+    for (int i in nearByTickets) {
+        if (!flatRules.contains(i)) {
+            sum += i;
+            indicesToBeRemove.add(i);
+        }
+    }
 
-    // print(colCount);
-    // print(nearByTickets.length / colCount);
+    print("Part 1 $sum");
+
+    List<Entry> entries = new List();
+
+    for(MapEntry<String, Set<int>> kvp in rules.entries) {
+        Entry entry = new Entry(kvp.key);
+        for(int i = 0; i < yourTickets.length; i++) {
+            List<int> col =
+                    getColumn(nearByTickets, yourTickets,
+                            indicesToBeRemove, yourTickets.length, i);
+
+            if (kvp.value.containsAll(col)) {
+                entry.add(i);
+            }
+        }
+        entries.add(entry);
+    }
+
+    entries.sort((a, b) => a.value.length - b.value.length);
+
+    for(int i = 0; i < entries.length; i++) {
+        for(int j = i + 1; j < entries.length; j++) {
+            List<int> duplicates = entries[i].value;
+            for (int duplicate in duplicates) {
+                entries[j].value.remove(duplicate);
+            }
+        }
+    }
+
+
+    int productOfDepartures = entries.fold(1, (acc, item) {
+        if (item.key.contains("departure")) {
+            acc *= yourTickets[item.value[0]];
+        }
+        return acc;
+    });
+
+    print("Part 2 : $productOfDepartures");
 }
